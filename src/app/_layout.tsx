a57dfe1bda048508edc679a9b9fe50589cd1d1f9
wonder of codes <grantjustinsilvaportela@gmail.com>
2026-09-17 08:42:46 -0300

    versão 4 do projeto AppTarefas.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,33 @@
-import { Stack } from "expo-router";
-import { StackTitle } from "expo-router/build/layouts/stack-utils";
 
-export default function layout () {
+//importar o componente que controla a navegação (rotas)
+import{ Stack } from 'expo-router';
+
+export default function Layout( ) {
   return(
     <Stack>
       <Stack.Screen
-      name= "index"
+      name='index'
       options={{
-        title:"agendadpr de tarefas"
+        title: "Agendador de Tarefas",
       }}
       />
 
-    <Stack.Screen
-    name='nova-tarefa'
-    options={{
-      title: "nova Tarefa"
-    }}
-    />
-
+      <Stack.Screen
+      name='nova-tarefa'
+      options={{
+        title: "Nova Tarefa",       
+      }}
+      />
     </Stack>
- 
-)
+  );
+  
 }
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useColorScheme } from 'react-native';
+
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
+
+SplashScreen.preventAutoHideAsync();
